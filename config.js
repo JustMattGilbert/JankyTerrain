a566d597eb7bcbecdeab7f48328a636f5d054fed
JustMattGilbert <matt@everyway.io>
2020-10-27 11:57:56 -0400

    First Commit

---
+++ config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  siteTitle: 'Janky Terrain', // <title>
+  manifestName: 'Janky Terrain',
+  manifestShortName: 'Janky Terrain', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'Janky Terrain',
+  subHeading:
+    "Website coming early 2021. Sign up to recieve updates and to be notified when we are live.",
+  socialLinks: [
+    {
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://www.instagram.com/jankyterrain/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/jankyterrain',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/jankyterrain',
+    },
+    {
+      icon: 'fa-youtube',
+      name: 'YouTube',
+      url: '#',
+    },
+    {
+      icon: 'fa-twitch',
+      name: 'Twitch',
+      url: '#',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:hello@jankyterrain.com',
+    },
+  ],
+};
